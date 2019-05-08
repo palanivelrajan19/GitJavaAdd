35f7d8603c235aa4eb8ab56e8e886284a057f97c
palanivelrajan19 <chan.raja@gmail.com>
2019-05-08 11:13:00 +0530

    added baseclass

---
+++ src/org/test/Samplepgm.java
@@ -0,0 +1,10 @@
+package org.test;
+
+public class Samplepgm {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Continue................");
+	}
+
+}
